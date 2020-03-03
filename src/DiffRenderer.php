531a1bc85e050533c15c53e67dd6860c6b1fdad3
VitaMinkin <inetmanager2009@gmail.com>
2020-03-04 00:40:56 +1000

    developed plain formatted

--- src/DiffRenderer.php
+++ src/DiffRenderer.php
@@ -75,8 +75,49 @@ class DiffRenderer
         return "{\n$outputString}\n";
     }
 
+
+    private static function isComplexValue($item)
+    {
+        if (is_array($item)) {
+            return 'complex value';
+        } else {
+            return $item;
+        }
+    }
+
     private static function convertToPlain(array $diff)
     {
-        return "called";
+        $converter = function ($diff, $parentName = '') use (&$converter) {
+            $result = array_reduce ($diff, function ($output, $element) use (&$converter, $parentName) {
+                $propertyName = "$parentName{$element['name']}";
+
+                $state = $element['diff'];
+
+                if (!empty($state)) {
+                    if (isset($state['value'])) {
+                        $value = self::isComplexValue($state['value']);
+                    } else {
+                        $oldValue = self::isComplexValue($state['oldValue']);
+                        $newValue = self::isComplexValue($state['newValue']);
+                    }
+
+                    if ($state['itemState'] === 'changed') {
+                        $output .= "Property '$propertyName' was changed. From '$oldValue' to '$newValue'" . "\n";
+                    } elseif ($state['itemState'] === 'added') {
+                        $output .= "Property '$propertyName' was added with value: '$value'" . "\n";
+                    } elseif ($state['itemState'] === 'deleted') {
+                        $output .= "Property '$propertyName' was removed" . "\n";
+                    }
+                }
+
+                if (!empty($element['children'])) {
+                    $output .= $converter($element['children'], "{$propertyName}.");
+                }
+
+                return $output;
+            }, '');
+            return $result;
+        };
+        return $converter($diff);
     }
 }
