3c34d120f144437d0fb5f92aa55391e1e80e9745
VitaMinKin <inetmanager2009@gmail.com>
2020-09-29 13:44:53 +1000

    Reduced cyclomatic complexity in getFormattedString function, rename variable  to  in converter function, added createString abstraction

--- src/Formatters/pretty.php
+++ src/Formatters/pretty.php
@@ -8,62 +8,67 @@ use const Differ\builder\DIFF_ELEMENT_NESTED;
 use const Differ\builder\DIFF_ELEMENT_UNCHANGED;
 use const Differ\builder\DIFF_ELEMENT_REMOVED;
 
-function getFormattedString($item, $depth)
+function getFormattedString($item, $indents)
 {
     if (is_bool($item)) {
         return boolval($item) ? 'true' : 'false';
     }
 
-    if (is_object($item)) {
-        $preResult = json_encode($item, JSON_PRETTY_PRINT);
-
-        $listOfStrings = explode("\n", $preResult);
-        $addPadding = array_map(function ($elem) use ($depth) {
-            return $depth . str_replace('"', "", $elem);
-        }, $listOfStrings);
-        $addPadding[0] = '{';
-
-        $result = implode("\n", $addPadding);
-        return $result;
+    if (!is_object($item)) {
+        return $item;
     }
 
-    return $item;
+    $preResult = json_encode($item, JSON_PRETTY_PRINT);
+
+    $listOfStrings = explode("\n", $preResult);
+    $addPadding = array_map(fn ($elem) => $indents . str_replace('"', "", $elem), $listOfStrings);
+    $addPadding[0] = '{';
+
+    $result = implode("\n", $addPadding);
+    return $result;
+}
+
+function createString($prefix, $key, $value)
+{
+    return $prefix . $key . ': ' . $value;
 }
 
 function convertToText(array $diff)
 {
-    $converter = function ($diff, $depth = "") use (&$converter) {
+    $converter = function ($diff, $indents = "") use (&$converter) {
         $prefix = [DIFF_ELEMENT_UNCHANGED => '    ', DIFF_ELEMENT_ADDED => '  + ', DIFF_ELEMENT_REMOVED => '  - '];
 
-        $depthPrefix = array_map(fn($item) => $depth . $item, $prefix);
+        $depthPrefix = array_map(fn($item) => $indents . $item, $prefix);
 
         $result = array_reduce($diff, function ($acc, $element) use (&$converter, $depthPrefix) {
 
-            ['name' => $elementName] = $element;
+            $elementName = $element['name'];
             [
-                DIFF_ELEMENT_UNCHANGED => $depth,
-                DIFF_ELEMENT_ADDED => $added,
-                DIFF_ELEMENT_REMOVED => $deleted
+                DIFF_ELEMENT_UNCHANGED => $depthIndent,
+                DIFF_ELEMENT_ADDED => $plusSign,
+                DIFF_ELEMENT_REMOVED => $minusSign
             ] = $depthPrefix;
 
             switch ($element['type']) {
                 case DIFF_ELEMENT_ADDED:
                 case DIFF_ELEMENT_REMOVED:
                 case DIFF_ELEMENT_UNCHANGED:
-                    $value = getFormattedString($element['value'], $depth);
-                    $acc[] = "{$depthPrefix[$element['type']]}{$elementName}: $value";
+                    $value = getFormattedString($element['value'], $depthIndent);
+                    $acc[] = createString($depthPrefix[$element['type']], $elementName, $value);
                     break;
                 case DIFF_ELEMENT_CHANGED:
-                    $oldValue = getFormattedString($element['oldValue'], $depth);
-                    $newValue = getFormattedString($element['newValue'], $depth);
-                    $acc[] = "{$added}{$elementName}: $newValue";
-                    $acc[] = "{$deleted}{$elementName}: $oldValue";
+                    $oldValue = getFormattedString($element['oldValue'], $depthIndent);
+                    $newValue = getFormattedString($element['newValue'], $depthIndent);
+                    $acc[] = createString($plusSign, $elementName, $newValue);
+                    $acc[] = createString($minusSign, $elementName, $oldValue);
                     break;
                 case DIFF_ELEMENT_NESTED:
-                    $acc[] = "{$depth}{$elementName}: {";
-                    $acc[] = $converter($element['children'], $depth);
-                    $acc[] = "$depth}";
+                    $acc[] = createString($depthIndent, $elementName, '{');
+                    $acc[] = $converter($element['children'], $depthIndent);
+                    $acc[] = $depthIndent . '}';
                     break;
+                default:
+                    throw new \Exception("Unknown type {$element['type']} in diff!");
             }
 
             return $acc;
